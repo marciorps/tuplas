01113876e4b61802628aa8913de46c8793b23811
marciorps <marciorps@yahoo.com,.br>
2023-12-07 11:35:10 -0300

    tuplas

---
+++ tuplas.py
@@ -0,0 +1,19 @@
+# Tuplas
+site1 = 'youtube.com'
+site2 = 'facebook.com'
+site3 = 'instagram.com'
+# ou
+valor1 = 23
+valor2 = 43
+valor3 = 65
+
+#criação de tuplas
+sites = ('youtube.com', 'facebook.com', 'instagram.com', 50,True)
+valores = (23,43,65,False)
+#União de tuplas
+dados_do_sistema = sites + valores
+print(dados_do_sistema)
+#Acesso de valores em uma tupla
+print(dados_do_sistema)
+print(sites)
+print(valores)
